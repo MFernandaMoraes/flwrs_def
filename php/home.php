96cbfde6ccf011e2bdaf8ea9e0595aea0ea20719
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-06-22 08:40:28 -0300

    link css na home

--- php/home.php
+++ php/home.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Flwrs · afeto em flor</title>
+  <link rel="stylesheet" href="../css/home.css">
 </head>
 <body>
   <header>
