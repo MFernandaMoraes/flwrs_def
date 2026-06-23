85e01e9a2d452ed668536c65c827fb6a7a8d81d8
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-06-23 10:25:11 -0300

    update

--- php/cad_produto.php
+++ php/cad_produto.php
@@ -180,6 +180,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>flwrs · cadastro de produtos</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0,1" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../css/cad_produto.css">
 </head>
 <body>
