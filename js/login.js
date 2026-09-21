673fc8ba42549cc5322c05a1c0c767181303c96c
MFernandaMoraes <ferreira.moraes.maria@escola.pr.gov.br>
2026-09-21 07:50:29 -0300

    T-T

--- js/login.js
+++ js/login.js
@@ -10,9 +10,3 @@ function togglePasswordVisibility(inputId, button) {
     icon.textContent = 'visibility';
     }
 }
-
-// (opcional) prevenir submit para exemplo
-document.querySelector('form')?.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('(demonstração) — em um site real, você seria autenticado.');
-});
